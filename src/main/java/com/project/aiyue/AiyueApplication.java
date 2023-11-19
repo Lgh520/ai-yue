383673f4d9a6e80283b9e946084665188e97399c
Lgh520 <lghao417@163.com>
2023-11-19 22:04:49 +0800

    修改

--- src/main/java/com/project/aiyue/AiyueApplication.java
+++ src/main/java/com/project/aiyue/AiyueApplication.java
@@ -1,8 +1,10 @@
 package com.project.aiyue;
 
+import com.github.pagehelper.autoconfigure.PageHelperAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 
 @ComponentScan("com.project.aiyue.*")
 @MapperScan("com.project.aiyue.dao")
-@SpringBootApplication
+@SpringBootApplication(exclude = {PageHelperAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class AiyueApplication {
 
     @RequestMapping
--- src/main/java/com/project/aiyue/controller/ClientController.java
+++ src/main/java/com/project/aiyue/controller/ClientController.java
@@ -1,12 +1,12 @@
 package com.project.aiyue.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller("/client")
+@RestController
 public class ClientController {
 
     @GetMapping("/getInfo")
