ffd72bcba3a26eb3122a4d02b2372e669a77afcd
Lgh520 <lghao417@163.com>
2023-11-25 16:32:16 +0800

    Merge branch 'main' of D:\ideaproject\ai-yue with conflicts.

--- src/main/java/com/project/aiyue/controller/UserInfoController.java
+++ src/main/java/com/project/aiyue/controller/UserInfoController.java
@@ -1,13 +1,15 @@
 package com.project.aiyue.controller;
 
 
-import com.project.aiyue.constant.ResponCodeConstant;
+import com.project.aiyue.ResponCodeConstant;
 import com.project.aiyue.dao.po.UserInfo;
+import com.project.aiyue.exception.CommonException;
 import com.project.aiyue.responor.CommonRespon;
 import com.project.aiyue.service.UserInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,21 +23,30 @@ public class UserInfoController {
     @Autowired
     private UserInfoService userInfoService;
 
-    @PostMapping ("/register")
-    @ApiOperation("客户端用户注册接口")
-    public CommonRespon register(@RequestBody @Valid UserInfo userInfo)  {
-        try {
-            String register = userInfoService.register(userInfo);
-            if(register != null){
-                return CommonRespon.success(register);
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-            log.info("用户注册失败：{}",e.getMessage());
-            return  CommonRespon.error(ResponCodeConstant.ERROR_CODE,e.getMessage());
-        }
-        return  CommonRespon.error(ResponCodeConstant.ERROR_CODE,"注册失败，请重试");
-    }
+//    @PostMapping ("/register")
+//    @ApiOperation("客户端用户注册接口")
+//    public CommonRespon register(@RequestBody @Valid UserInfo userInfo)  {
+//        try {
+//            String register = userInfoService.register(userInfo);
+//            if(register != null){
+//                respon.setCode(ResponCodeConstant.NORMAL_CODE);
+//                respon.setData(register);
+//            }
+//        }catch (CommonException e){
+//            e.printStackTrace();
+//            log.info(e.getMessage());
+//            respon.setCode(ResponCodeConstant.PARA_ERROR_CODE);
+//            respon.setData(e.getMessage());
+//            return respon;
+//        }catch (Exception e){
+//            e.printStackTrace();
+//            log.info(e.getMessage());
+//            respon.setCode(ResponCodeConstant.PARA_ERROR_CODE);
+//            respon.setData(e.getMessage());
+//            return respon;
+//        }
+//        return respon;
+//    }
 
     @ApiOperation("客户端用户登录接口")
     @PostMapping ("/login")
